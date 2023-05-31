3d88af96193218f9f39f7c34cce7f5706f788269
kevinjapa <84851902+kevinjapa@users.noreply.github.com>
2023-05-31 08:40:33 -0500

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,9 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatNativeDateModule} from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -44,7 +47,9 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatButtonModule,
     MatNativeDateModule,
-    MatIconModule
+    MatIconModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'contacto62-7e085',
+    appId: '1:1054445271227:web:253937c817b77c412a919e',
+    storageBucket: 'contacto62-7e085.appspot.com',
+    apiKey: 'AIzaSyAxBkvJaeRGUkBYiDnig1PEogfG767Ytkg',
+    authDomain: 'contacto62-7e085.firebaseapp.com',
+    messagingSenderId: '1054445271227',
+  },};
